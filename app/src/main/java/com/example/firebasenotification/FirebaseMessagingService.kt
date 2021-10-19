faa5a9168b6256fe36fcaee4607bd2d628761466
ASUS <dhirinder@opayn.com>
2021-10-19 17:57:26 +0530

    firebase message notification get

---
+++ app/src/main/java/com/example/firebasenotification/FirebaseMessagingService.kt
@@ -0,0 +1,92 @@
+package com.example.firebasenotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    val TAG = "ServiceMessage123"
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
+        sendNotification(remoteMessage.notification?.body!!)
+        if (remoteMessage.getNotification() != null) {
+            // Since the notification is received directly from
+            // FCM, the title and the body can be fetched
+            // directly as below.
+
+
+        }
+
+
+
+
+    // Check if message contains a notification payload.
+    remoteMessage.notification?.let {
+        Log.d(TAG, "Message Notification Body: ${it.body}")
+    }
+
+
+    }
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token)
+
+    }
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+
+
+    private fun sendNotification(messageBody: String) {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+            .setContentTitle(getString(R.string.fcm_message))
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    companion object {
+
+        private const val TAG = "MyFirebaseMsgService"
+    }
+
+}
---
+++ app/src/main/java/com/example/firebasenotification/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.firebasenotification
+
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("StringFormatInvalid")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
+       runtimeEnableAutoInit()
+    }
+    fun runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        Firebase.messaging.isAutoInitEnabled = true
+        // [END fcm_runtime_enable_auto_init]
+    }
+}
